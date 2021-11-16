64a9d0253ac39317fa5b62f686ba30afad6a3220
Arian Abedanzadeh <arianabedan@gmail.com>
2021-11-16 13:46:09 -0500

    Add artist search and use real artist data for the artist page

--- app.py
+++ app.py
@@ -320,16 +320,25 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    # data = [{
+    #     "id": 4,
+    #     "name": "Guns N Petals",
+    # }, {
+    #     "id": 5,
+    #     "name": "Matt Quevedo",
+    # }, {
+    #     "id": 6,
+    #     "name": "The Wild Sax Band",
+    # }]
+    artists = Artist.query.all()
+
+    data = []
+
+    for artist in artists:
+        data.append({
+            'id': artist.id,
+            'name': artist.name
+        })
     return render_template('pages/artists.html', artists=data)
 
 
@@ -338,14 +347,20 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+    artists = Artist.query.filter(
+        Artist.name.ilike('%' + search_term + '%')).all()
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(artists),
+        "data": []
     }
+
+    for artist in artists:
+        response['data'].append({
+            'id': artist.id,
+            'name': artist.name,
+        })
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
